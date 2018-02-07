9352543a71db62ac49253d11756496735d430dab
phgoff <mous17zy@hotmail.com>
2018-02-07 13:38:25 +0700

    Edit Lab3

--- LAB3/StudentList.java
+++ LAB3/StudentList.java
@@ -1,21 +1,31 @@
+import java.util.*;
 public class StudentList{
+		private ArrayList<Student> stlist;
 
 		public	StudentList(){
+			this.stlist = new ArrayList<Student>();
 		}
 
-		public addStudent(){
-
+		public void addStudent(Student std){
+			this.stlist.add(std);
 		}
 
+		public int searchStudentBystCode(String stCode){
+			for(int i=0; i<this.stlist.size(); i++){
+				if (this.stlist.get(i).getCode() == stCode){
+					return i;
+				}
+			}
+
+		}
 		public removeStudentBystCode(String stCode){
-
 		}
+
 		public ShowAllStudentinfo(){
-
 		}
-		public searchStudentBystCode(){
 
-		}
+		
+
 		public getNumberofStudent(){
 		}
 
