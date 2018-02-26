1f91421beaf45a8a8adc99f79dd396d8fce67da4
phgoff <mous17zy@hotmail.com>
2018-02-26 12:29:53 +0700

    Edited

--- LAB4/ExtractIt.java
+++ LAB4/ExtractIt.java
@@ -3,19 +3,17 @@ public class ExtractIt{
 	public static String extract(String url){
 		String[] str= url.split("/");
 		String name = str[str.length-1 ];
+		int len = name.length()- 1;
 
 		if(name.startsWith("*"))
-			name = "_" + name.substring(1,name.length()-1);
-
+			name = "_" + name.substring(1,len);
 		return name;
-
 	}
 	
 	public static void main(String[] args) {
+		
 		System.out.println(extract("http://example.com/files/public_files/test.zip"));
 		System.out.println(extract("www.abc.com/pub/*aaa.html"));
-
 	}
-
 
 }
