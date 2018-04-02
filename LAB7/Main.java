6d2189199a1ebfdda62b9203d6d0027c47b90843
phgoff <mous17zy@hotmail.com>
2018-04-02 12:24:29 +0700

    Create Main.java

---
+++ LAB7/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+	public static void main(String[] args) {
+		new SwingCounter();
+		//new SwingCounter2();
+		//new SwingCounter3();
+	}
+}
+
+// -----------------------------------------------------
+
+// public class Main{
+
+// 	public static void main(String[] args) {
+		
+// 		SwingUtilities.invokeLater(new Runnable(){
+
+// 				public void run(){
+// 					//new SwingCounter();
+// 		 			// new SwingCounter2();
+// 					new SwingCounter3();
+// 				}
+// 		});
+		
+// 	} //end main
+// } //end class
+
+// -----------------------------------------------------
